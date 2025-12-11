2cae09f0df371a15c74fef5fc9fb86d395b2d1cb
zambar <zambar@shanghaitech.edu.cn>
2025-12-11 22:52:45 +0800

    feat: custom port

--- server.ts
+++ server.ts
@@ -150,7 +150,9 @@ app.prepare().then(() => {
     });
   });
 
-  server.listen(3000, () => {
-    console.log('> Ready on http://localhost:3000');
+  const port = process.env.PORT || 3000;
+  
+  server.listen(port, () => {
+    console.log(`> Ready on http://localhost:${port}`);
   });
 });
