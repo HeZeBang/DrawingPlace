f13137a20edec7bb7009dd82c484d7056c17f60c
zambar <zambar@shanghaitech.edu.cn>
2025-12-24 03:16:18 +0800

    chore: v2.2.5

--- components/AnnounceModal.tsx
+++ components/AnnounceModal.tsx
@@ -29,10 +29,10 @@ const AnnounceContent = () => {
     <div className="prose p-4 leading-tight prose-sm">
       <p>
         各位玩家朋友们好！我们的新年绘板在短短两天正式上线就突破了 100 人注册和
-        30,000 次绘制！可喜可贺！
+        40,000 次绘制！可喜可贺！🥳🥳🥳
       </p>
       <p>
-        随着新年越来越近，为了公平起见，我们也会随时间逐渐提高绘画成本，避免覆盖。本次版本更新带来了以下变化和改进：
+        随着新年越来越近，如之前所说，为了公平起见，我们也会随时间逐渐提高绘画成本，避免覆盖。本次版本更新带来了以下变化和改进：
       </p>
       <ul className="list-disc pl-5">
         <li>背包容量缩小至 96，冷却时间提升至 3 秒。</li>
@@ -43,6 +43,13 @@ const AnnounceContent = () => {
             target="_blank"
           >
             GeekPie 博客页面
+          </a>
+          或者
+          <a
+            href="https://ohmygpa.icu/topic/11/2025-%E6%96%B0%E5%B9%B4%E7%BB%98%E6%9D%BF-%E8%A7%84%E5%88%99%E8%AF%B4%E6%98%8E-api-%E6%96%87%E6%A1%A3%E5%8F%8A%E6%A0%B7%E4%BE%8B"
+            target="_blank"
+          >
+            OhMyGPA 论坛专栏
           </a>
           查看。
         </li>
@@ -55,11 +62,22 @@ const AnnounceContent = () => {
         </a>{" "}
         开设了专栏，
         <b>
-          欢迎大家在上面发起组队或者发送反馈。之后我们的评比和开奖也会在这个平台进行
+          欢迎大家在上面发起组队招募令或者发送反馈。之后我们的
+          <b>
+            <a
+              href="https://ohmygpa.icu/topic/12/2025-%E6%96%B0%E5%B9%B4%E7%BB%98%E6%9D%BF-%E6%B4%BB%E5%8A%A8%E4%BB%8B%E7%BB%8D"
+              target="_blank"
+            >
+              投票评选和开奖环节
+            </a>
+          </b>
+          也会在这个平台进行
         </b>
         ，敬请期待！
       </p>
-      <p>P.S. 论坛直接使用绘图的 GeekPie 账号即可免注册登录！</p>
+      <p>
+        P.S. 论坛直接使用绘图的 GeekPie 账号即可<b>免注册登录！</b>
+      </p>
       <p>感谢大家的支持与反馈！祝大家玩得开心！</p>
       <p>🎄圣诞愉快！🎄</p>
     </div>
