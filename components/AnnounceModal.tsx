a737706e2517b71c85f6d3fa083386e30997450c
zambar <zambar@shanghaitech.edu.cn>
2025-12-27 03:24:31 +0800

    doc: update announce

--- components/AnnounceModal.tsx
+++ components/AnnounceModal.tsx
@@ -28,16 +28,29 @@ const AnnounceContent = () => {
   return (
     <div className="prose p-4 leading-tight prose-sm">
       <p>
-        各位玩家朋友们好！我们的新年绘板在短短两天正式上线就突破了 100 人注册和
-        40,000 次绘制！可喜可贺！🥳🥳🥳
+        大家好！我们很高兴地宣布，绘板应用已更新至 v
+        {process.env.NEXT_PUBLIC_APP_VERSION} 版本！
       </p>
-      <p>
-        随着新年越来越近，如之前所说，为了公平起见，我们也会随时间逐渐提高绘画成本，避免覆盖。本次版本更新带来了以下变化和改进：
-      </p>
+      <p>本次版本更新带来了以下变化和改进：</p>
       <ul className="list-disc pl-5">
-        <li>背包容量缩小至 96，冷却时间提升至 3 秒。</li>
         <li>
-          我们发布了官方 API 手册和 Python 例程，前往{" "}
+          <s>修复已知问题、提升用户体验。</s>
+        </li>
+        <li>背包容量缩小至 64，冷却时间提升至 5 秒。</li>
+        <li>
+          现在新的 API 允许你省略 <code>h</code>, <code>w</code>，
+          <code>token</code>参数。
+        </li>
+        <li>
+          之前的<code>/api/place</code>将标记为废弃，建议使用新的 API 端点{" "}
+          <code>/api/v2/place</code>，这将省略宽高参数从而减少初始化时间。
+        </li>
+        <li>
+          前端使用了新的 API 端点<code>/api/v2/init</code>
+          ，使用二进制格式减少开销，并支持增量更新。
+        </li>
+        <li>
+          我们也同步<b>更新了</b>官方 API 手册，前往{" "}
           <a
             href="https://geekpie.club/posts/blog/2025-12-22-paint2025/"
             target="_blank"
@@ -54,32 +67,7 @@ const AnnounceContent = () => {
           查看。
         </li>
       </ul>
-
-      <p>
-        此外，我们在合作论坛{" "}
-        <a href="https://ohmygpa.icu/category/9/paint" target="_blank">
-          OhMyGPA 论坛
-        </a>{" "}
-        开设了专栏，
-        <b>
-          欢迎大家在上面发起组队招募令或者发送反馈。之后我们的
-          <b>
-            <a
-              href="https://ohmygpa.icu/topic/12/2025-%E6%96%B0%E5%B9%B4%E7%BB%98%E6%9D%BF-%E6%B4%BB%E5%8A%A8%E4%BB%8B%E7%BB%8D"
-              target="_blank"
-            >
-              投票评选和开奖环节
-            </a>
-          </b>
-          也会在这个平台进行
-        </b>
-        ，敬请期待！
-      </p>
-      <p>
-        P.S. 论坛直接使用绘图的 GeekPie 账号即可<b>免注册登录！</b>
-      </p>
       <p>感谢大家的支持与反馈！祝大家玩得开心！</p>
-      <p>🎄圣诞愉快！🎄</p>
     </div>
   );
 };
@@ -98,14 +86,13 @@ const AnnounceModal = ({
   useEffect(() => {
     if (!isOpen) return;
     const end = Date.now() + 3 * 1000; // 3 seconds
-    // const colors = ["#a786ff", "#fd8bbc", "#eca184", "#f8deb1"];
-    const colors = ["#228B22", "#FF0000", "#FFFF00", "#FFA500", "#FFFFFF"]; // Christmas colors
+    const colors = ["#a786ff", "#fd8bbc", "#eca184", "#f8deb1"];
 
     const frame = () => {
       if (Date.now() > end) return;
 
       confetti({
-        particleCount: 6,
+        particleCount: 4,
         angle: 60,
         spread: 55,
         startVelocity: 60,
@@ -113,7 +100,7 @@ const AnnounceModal = ({
         colors: colors,
       });
       confetti({
-        particleCount: 6,
+        particleCount: 4,
         angle: 120,
         spread: 55,
         startVelocity: 60,
